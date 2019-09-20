3f8898781c5b5a41781f9e2d5967465d8c9de9f5
github username <github email address>
2019-09-20 09:15:16 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,10 +3,29 @@ class StudentsController < ApplicationController
   
   def index
     @students = Student.all
+
   end
 
   def show
+    @student = Student.find(params[:id])
+    if @student.active == false
+      @response = "This student is currently inactive."
+    else
+      @response = "This student is currently active."
+    end
   end
+
+    def active
+      @student = Student.find(params[:id])
+      if @student.active == false
+        @student.update(active: true)
+      else
+        @student.update(active: false)
+      end
+    redirect_to student_path(@student)
+    end
+    
+
 
   private
 
