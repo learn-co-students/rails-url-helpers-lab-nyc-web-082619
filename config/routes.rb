6adc503c268eb76b7a3c4c90eaacb6383113833b
Eugenia Kim <elk303@nyu.edu>
2019-09-20 21:12:34 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'students', to: 'students#index' 
+  # get is the request; students matches the http request and goes to the students controller where the index method 
+  # implemented in the view page
+  # http verb; match http; to:; controller you want # method in the controller; (optional sometimes prefix to the path)
+  get 'students/:id', to: 'students#show', as: 'student'
+  get 'students/:id/activate', to: 'students#activate', as: "activate_student"
 end
